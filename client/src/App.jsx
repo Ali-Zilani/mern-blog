caae9ede72aedd2199766f26fa3e724fed919458
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-15 23:16:45 +0530

    install reactjs and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className='font-bold text-3xl text-green-600 bg-red-200 text-center'>This is Mern Blog APP </h1>
+       </>
+  )
+}
+
+export default App
