1762ab22e6be9fe73b1128115ed4c8d4813d1e5d
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-16 22:59:28 +0530

    add functionality to signup page

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,8 +2,6 @@ import express from 'express'
 const router = express.Router();
 import {signup} from '../controllers/auth.controller.js'
 
-
-
 router.post('/signup', signup)
 
-export default router ;
+export default router ; 
