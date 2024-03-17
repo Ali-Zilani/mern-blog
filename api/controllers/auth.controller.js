c3b51705b69aeda2db3a3403734b57bfbcb351a8
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-17 12:01:15 +0530

    create sigin api route , generated cookie jst token

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next)=>{
     //console.log(req.body)
@@ -8,7 +9,7 @@ export const signup = async (req,res,next)=>{
 
     if(!username || !email || !password || username==='' || email==='' || password===''){
        // return res.status(400).json({message: 'All fields are reqiured'}) // client side error
-       next(errorHandler(400,'All fields are required'))
+       return  next(errorHandler(400,'All fields are required'))
     }
 
     // hashing the password
@@ -28,5 +29,35 @@ export const signup = async (req,res,next)=>{
     catch(err){ // server side error
         next(err) // using middleware to handle error
         //res.status(500).json({message:`Error in user signup ${err}`})
+    }
+};
+
+
+export const signin = async (req, res, next) =>{
+    const {email, password} = req.body ;
+
+    if(!email || !password || email === '' || password === '' ){
+        return next(errorHandler(400,'All fields are required'))
+    }
+
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404,'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            return next(errorHandler(400,'Invalid Password'))
+        }
+        // to get info about user without password info
+        const {password:pass, ...rest} = validUser._doc;
+
+        // authenctication using jsonwebtoken lirary as jwt
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true}).json(rest)
+    }
+    catch(err){
+        next(err);
     }
 }
