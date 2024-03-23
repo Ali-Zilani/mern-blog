f4009980bef53c4a8a0d455a85c96a8dbb37cab6
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-23 17:14:14 +0530

    add update user api router

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO)
 .then(()=>{console.log("DB is Connectected successfully!")})
