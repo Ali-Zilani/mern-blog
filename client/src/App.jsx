3cb928195b23e7041f8085e02d5abd27551de9ff
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-03-29 22:45:09 +0530

    Show users to admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         </Route>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
